5250a3ea45fe04762a67e326bea7528638e4d1f5
Evans Frimpong <ezfrimps@gmail.com>
2021-12-12 21:54:19 -0500

    first module

---
+++ my_addons/my_library/__manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name': 'My Library module',
+  'summary': "Manage books easily",
+  'description': "",
+  'author': "Evans Frimpong",
+  'website': "https://www.sysloggh.com",
+  'category': "Uncategorized",
+  'version': '1.0.0',
+  'depends': ['base'],
+  # 'data': ['views/views.xml'],
+  # 'demo': ['demo.xml']
+}
